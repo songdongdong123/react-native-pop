0f3cf0393fe3a91a6550feb5f13d7657ea0896ac
ethan <172529131@qq.com>
2019-03-12 14:18:58 +0800

    android端物理返回键的处理

--- js/redux/store/index.js
+++ js/redux/store/index.js
@@ -1,10 +1,22 @@
 import {applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducer';
-import {middleware} from '../../navigator/AppNavigator'
+import {middleware} from '../../navigator/AppNavigator';
+
+const logger = store =>next=>action=>{
+  if (typeof action === 'function') {
+    console.log('dispatching a function');
+  } else {
+    console.log('dispatching', action)
+  }
+  const result = next(action);
+  console.log('nextState', store.getState());
+}
 
 const middlewares = [
-  middleware
+  middleware,
+  logger,
+  thunk
 ];
 
 /** 
